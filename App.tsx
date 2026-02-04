b93b75b343e9aa4f50c0533e2ecdc2eda4a64a07
shilpaprapatil-creator <shilpa.prapatil@gmail.com>
2026-02-04 21:15:16 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,40 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Philosophy from './components/Philosophy';
-import Flagship from './components/Flagship';
-import CentersOfExcellence from './components/COE';
-import IndustryIntegration from './components/Industry';
-import Impact from './components/Impact';
-import Footer from './components/Footer';
-
-const App: React.FC = () => {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
+export default function App() {
   return (
-    <div className="flex flex-col min-h-screen selection:bg-cyan-500/30">
-      <Navbar scrolled={scrolled} />
-      <main>
-        <Hero />
-        <About />
-        <Philosophy />
-        <Flagship />
-        <CentersOfExcellence />
-        <IndustryIntegration />
-        <Impact />
-      </main>
-      <Footer />
+    <div style={{ color: "white", padding: "40px" }}>
+      <h1>Dr. Prakash Sheelvanthmath</h1>
+      <p>Pro Vice Chancellor</p>
     </div>
-  );
-};
-
-export default App;
+  )
+}
