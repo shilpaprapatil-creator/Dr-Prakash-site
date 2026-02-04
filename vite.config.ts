a04f0e3679c98835652a19645bf9ada4c2ca069a
shilpaprapatil-creator <shilpa.prapatil@gmail.com>
2026-02-04 21:02:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Using './' ensures that assets are loaded correctly regardless of the subpath
-  base: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
-});
+  base: '/Dr-Prakash-site/',  
+})
